448cedaeb23bd1092d6870f4d013919d83dedacb
ChessmawOgami <chessmawogami@gmail.com>
2023-08-05 13:52:55 -0600

    EndPage

--- src/PageEnd.jsx
+++ src/PageEnd.jsx
@@ -1,12 +1,15 @@
+import {BsGithub} from 'react-icons/bs'
 export function PageEnd() {
   return (
-    <div className="bg-black text-white font-Montserrat font-bold" >
-      <div className=" ">
+    <div className="" >
+      <div className="bg-black text-white font-Montserrat font-bold m-5 p-5 flex border-solid border-2 border-white shadow-lg shadow-slate-500/50">
+        <div className='flex'>
+        <BsGithub className='m-5 p-1' size={50} />
+        </div>
         <ul>
-          <li><a target="_blank" href="https://github.com/Chessmaw">Chessmaw</a></li>
-          <li><a target="_blank" href="https://github.com/ChessmawOgami">ChessmawOgami</a></li>
-          <li><a target="_blank" href="#">Blog</a></li>
-          <li><a target="_blank" href="#">More</a></li>
+          <li><a className='m-5  p-5 hover:text-orange-500' target="_blank" href="https://github.com/Chessmaw">Chessmaw</a></li>
+          <li><a className='m-5  p-5 hover:text-teal-500' target="_blank" href="https://github.com/ChessmawOgami">ChessmawOgami</a></li>
+          <li><a className='m-5  p-5 hover:text-orange-500' target="_blank" href="#">@By Creator Alejandro</a></li>
         </ul>
       </div>
     </div>
--- src/TargetName.jsx
+++ src/TargetName.jsx
@@ -1,6 +1,6 @@
 export function TargetName() {
   return (
-    <div className="m-5 group bg-black text-center rounded-lg">
+    <div className="m-5 group bg-black text-center rounded-lg font-Montserrat">
       <h1 className="mb-1 text-4xl text-white md:text-6xl">
         Hi, I'm <br className="block md:hidden" />
         <span className="inline-flex h-20 pt-2 overflow-x-hidden animate-type group-hover:animate-type-reverse whitespace-nowrap text-brand-accent will-change-transform">
