01231288f28ccd3d5d50a1d5fa983a167263e815
ChessmawOgami <chessmawogami@gmail.com>
2023-08-05 14:08:49 -0600

    UPDATE

--- src/PageEnd.jsx
+++ src/PageEnd.jsx
@@ -2,7 +2,7 @@ import {BsGithub} from 'react-icons/bs'
 export function PageEnd() {
   return (
     <div className="" >
-      <div className="bg-black text-white font-Montserrat font-bold m-5 p-5 flex border-solid border-2 border-white shadow-lg shadow-slate-500/50">
+      <div className="bg-black text-white font-Montserrat font-bold m-5 p-5 flex border-solid border-2 border-white shadow-lg shadow-slate-500/50 rounded-lg">
         <div className='flex'>
         <BsGithub className='m-5 p-1' size={50} />
         </div>
